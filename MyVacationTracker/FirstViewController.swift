8c2a155ada61f9def52d573ee837d2ffeaf0a487
Kim D Barnes <kimdbarnes@gmail.com>
2014-12-27 08:26:27 -0700

    Initial Commit

---
+++ MyVacationTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyVacationTracker
+//
+//  Created by Kimberly Barnes on 12/27/14.
+//  Copyright (c) 2014 DKMMBarnes, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ MyVacationTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyVacationTracker
+//
+//  Created by Kimberly Barnes on 12/27/14.
+//  Copyright (c) 2014 DKMMBarnes, LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
